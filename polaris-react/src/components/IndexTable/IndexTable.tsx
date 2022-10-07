50cbda30f990644cc3919f626bac6285c1c3138e
Marc Thomas <marc.thomas@shopify.com>
2022-10-07 18:17:56 +0100

    add entrance animations

--- polaris-react/src/components/IndexTable/IndexTable.tsx
+++ polaris-react/src/components/IndexTable/IndexTable.tsx
@@ -562,6 +562,7 @@ function IndexTableBase({
         promotedActions={promotedActions}
         actions={actions}
         onSelectModeToggle={condensed ? handleSelectModeToggle : undefined}
+        isSticky={isBulkActionsSticky}
       />
     </div>
   ) : null;
